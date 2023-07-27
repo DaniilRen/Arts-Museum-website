7bd65535bd2e787d3c46cb1e78cbc7e3d68045c5
Daniil Bykh <109330484+DaniilRen@users.noreply.github.com>
2023-07-27 18:33:29 +0300

    modal window

--- src/app.js
+++ src/app.js
@@ -1,10 +1,16 @@
 const contacts = document.querySelector('.contacts')
 const contacts_modal = document.querySelector('.contacts-modal')
+const modal_close = document.querySelector('.modal-close')
 
 contacts.addEventListener('click', () => {
-	contacts_modal.classList.toggle('hidden')
+	contacts_modal.classList.add('visible')
 })
+
+modal_close.addEventListener('click', () => {
+	contacts_modal.classList.remove('visible')
+})
+
 
 document.addEventListener('mousemove', e => {
 	document.body.style.cssText = `--move-x: ${e.clientX}px; --move-y: ${e.clientY}px`
-})
+})	
