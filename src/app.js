a0fae0fc7cfcdb7d3b347efc2e72120e58e0a1a5
Daniil Bykh <109330484+DaniilRen@users.noreply.github.com>
2023-08-03 22:04:22 +0300

    -

--- src/app.js
+++ src/app.js
@@ -1,16 +1,27 @@
 const contacts = document.querySelector('.contacts')
 const contacts_modal = document.querySelector('.contacts-modal')
 const modal_close = document.querySelector('.modal-close')
+const modal_overlay = document.querySelector('.modal-overlay')
+let html = document.documentElement;
+
 
 contacts.addEventListener('click', () => {
-	contacts_modal.classList.add('visible')
+	contacts_modal.classList.add('visible');
+	modal_overlay.classList.add('overlay-visible');
+	document.body.style.position = 'fixed';
+	document.body.style.top = `-${window.scrollY}px`;
 })
 
 modal_close.addEventListener('click', () => {
-	contacts_modal.classList.remove('visible')
+	contacts_modal.classList.remove('visible');
+	modal_overlay.classList.remove('overlay-visible');
 })
 
+modal_overlay.addEventListener('click', () => {
+	contacts_modal.classList.remove('visible');
+	modal_overlay.classList.remove('overlay-visible');
+})
 
 document.addEventListener('mousemove', e => {
 	document.body.style.cssText = `--move-x: ${e.clientX}px; --move-y: ${e.clientY}px`
-})	
+})
