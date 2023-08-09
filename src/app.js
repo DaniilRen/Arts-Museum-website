227495c5402c2cb86f3cee74728463efc6c1bd2c
Daniil Bykh <109330484+DaniilRen@users.noreply.github.com>
2023-08-09 20:23:31 +0300

    -

--- src/app.js
+++ src/app.js
@@ -1,26 +1,44 @@
-const contacts = document.querySelector('.contacts')
-const contacts_modal = document.querySelector('.contacts-modal')
-const modal_close = document.querySelector('.modal-close')
-const modal_overlay = document.querySelector('.modal-overlay')
-let html = document.documentElement;
-
-
 document.addEventListener('mousemove', e => {
 	document.body.style.cssText = `--move-x: ${e.clientX}px; --move-y: ${e.clientY}px`
 })
 
 
-contacts.addEventListener('click', () => {
-	contacts_modal.classList.add('visible');
-	modal_overlay.classList.add('overlay-visible');
-})
 
-modal_close.addEventListener('click', () => {
-	contacts_modal.classList.remove('visible');
-	modal_overlay.classList.remove('overlay-visible');
-})
+const contacts = document.querySelector('.contacts')
+contacts.addEventListener('click', add_modal_to_DOM)
 
-modal_overlay.addEventListener('click', () => {
-	contacts_modal.classList.remove('visible');
-	modal_overlay.classList.remove('overlay-visible');
-})
+
+function add_modal_to_DOM() {
+	document.body.insertAdjacentHTML('beforeEnd', modal.html)
+	const close_timeout_ms = 300
+	const open_timeout_ms = 0.00000000001
+
+	const contacts_modal = document.querySelector('.contacts-modal')
+
+	const modal_close = document.querySelector('.modal-close')
+	modal_close.addEventListener('click', () => {
+		contacts_modal.classList.remove('visible');
+		modal_overlay.classList.remove('overlay-visible');
+		setTimeout(() => {
+			contacts_modal.remove()
+			modal_overlay.remove()
+		}, close_timeout_ms);
+	})
+
+	const modal_overlay = document.querySelector('.modal-overlay')
+	modal_overlay.addEventListener('click', () => {
+		contacts_modal.classList.remove('visible');
+		modal_overlay.classList.remove('overlay-visible');
+		setTimeout(() => {
+			contacts_modal.remove()
+			modal_overlay.remove()
+		}, close_timeout_ms);
+	})
+
+	setTimeout(() => {
+		contacts_modal.classList.add('visible');
+		modal_overlay.classList.add('overlay-visible');
+	}, open_timeout_ms);
+	
+}
+
---
+++ src/modal.js
@@ -0,0 +1,15 @@
+const modal = {
+  html: `
+	<div class="contacts-modal">
+		<div class="modal-content">
+			<div class="question-1">Как с нами связаться?</div>
+			<div class="modal-element">Наш номер - 89147316762</div>
+			<div class="modal-element">Наша почта - fffn12jk@gmail.com</div>
+			<div class="question-2">Где нас можно найти?</div>
+			<div class="modal-element">Наш адрес - Проспект Маршала Жукова, д. 35, литера А, к. 3, кв. 105</div>
+		</div>
+		<div class="modal-close">+</div>
+	</div>
+	<div class="modal-overlay"></div>
+  `
+}
